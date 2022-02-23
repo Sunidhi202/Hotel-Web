86c1b92c8da57a30f22e7260cf0c2c726f15fbf6
Sunidhi202 <85990996+Sunidhi202@users.noreply.github.com>
2022-02-23 11:44:40 +0530

    Add files via upload

---
+++ hotel/urls.py
@@ -0,0 +1,62 @@
+"""hotel URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import sinha.views as v
+iews
+urlpatterns = [
+    path('', views.homepage,name="homepage"),
+    path('home', views.homepage,name="homepage"),
+    path('about', views.aboutpage,name="aboutpage"),
+    path('contact', views.contactpage,name="contactpage"),
+    path('user', views.user_log_sign_page,name="userloginpage"),
+    path('user/login', views.user_log_sign_page,name="userloginpage"),
+    path('terms', views.term, name="terms"),
+    
+    # Payment APIs
+    path('payment/', views.payment, name = 'payment'),
+    path('handlerequest/', views.handlerequest, name = 'handlerequest'),
+    # Generating Invoice
+    path('generateinvoice/<int:pk>/', views.GenerateInvoice.as_view(), name = 'generateinvoice'),
+
+    path('user/bookings', views.user_bookings,name="dashboard"),
+    path('user/book-room/<int:roomid>/<str:cin>/<str:cout>', views.book_room_page,name="bookroompage"),
+    path('user/book-room/book', views.book_room,name="bookroom"),
+    path('user/signup', views.user_sign_up,name="usersignup"),
+    path('staff/', views.staff_log_sign_page,name="staffloginpage"),
+    path('staff/login', views.staff_log_sign_page,name="staffloginpage"),
+    path('staff/signup', views.staff_sign_up,name="staffsignup"),
+    path('logout', views.logoutuser,name="logout"),
+    path('staff/panel', views.panel,name="staffpanel"),
+    path('staff/allbookings', views.all_bookings,name="allbookigs"),
+    path('staff/inventory', views.inventory,name="inventory"),
+    path('staff/panel/add-new-item', views.add_new_item,name="additem"),
+
+    path('staff/panel/edit-item/<int:id>', views.edit_item),
+    path('staff/panel/edit-item/edit', views.edit_item),
+    # path('staff/panel/view-item', views.view_item),
+
+    path('staff/panel/add-new-location', views.add_new_location,name="addnewlocation"),
+    path('staff/panel/edit-room', views.edit_room),
+    path('staff/panel/add-new-room', views.add_new_room,name="addroom"),
+    path('staff/panel/edit-room/edit', views.edit_room),
+    path('staff/panel/view-room', views.view_room),
+
+    path('admin/', admin.site.urls),
+    
+
+]
+
